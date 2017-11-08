9f4092473758e94be70965f701aca65fe0c5a8c9
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2017-11-09 03:39:42 +0900

    Test Realm object database with sample object.

--- ReturnOfTheAPI/ViewController.swift
+++ ReturnOfTheAPI/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let realm = try? Realm()
+
+        if let realm = realm {
+            saveCharacter(to: realm)
+            let characters = realm.objects(Character.self)
+            print(characters.first)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func testApi() {
         let client = SwapiClient()
         client.retrieveCharacter(with: 1) { character, error in
             guard let character = character else {
@@ -23,7 +39,7 @@ class ViewController: UIViewController {
 
         }
         var characterList: [Character] = []
-        var paginatedResults: Bool = false
+//        var paginatedResults: Bool = false
         client.retrievePaginatedCharacters { characters, error in
             guard let characters = characters else {
                 print(error as Any)
@@ -34,9 +50,18 @@ class ViewController: UIViewController {
         }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func saveCharacter(to realm: Realm) {
+        let client = SwapiClient()
+        client.retrieveCharacter(with: 1) { character, error in
+            guard let character = character else {
+                print("Ruh roh")
+                print(error as Any)
+                return
+            }
+            try? realm.write {
+                realm.add(character)
+            }
+        }
     }
 
 }
