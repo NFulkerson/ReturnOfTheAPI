c811913a44a0abd634468f8ad690f9a01de96ea2
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2017-12-18 02:24:27 +0900

    Removed stub file from app mockup process Removed non-generic/extraneous function from SWAPIClient retrieveCharacter(with resourceId:)

--- ReturnOfTheAPI/Networking/SWAPIClient.swift
+++ ReturnOfTheAPI/Networking/SWAPIClient.swift
@@ -29,29 +29,6 @@ class SwapiClient {
         retrieveResources(at: endpoint)
     }
 
-    func retrieveCharacter(with resourceId: Int,
-                           completion: @escaping (Character?, SwapiError?) -> Void) {
-
-        let endpoint = Swapi.request(resource: .character, id: resourceId)
-
-        performRequest(with: endpoint) { data, error in
-
-            guard let data = data else {
-                completion(nil, error)
-                return
-            }
-
-            let decoder = JSONDecoder()
-
-            do {
-                let character = try decoder.decode(Character.self, from: data)
-                completion(character, nil)
-            } catch {
-                completion(nil, .decodingFailed(message: String(describing: error)))
-            }
-        }
-    }
-
     private func retrieveResources(at endpoint: Endpoint) {
         let fetch = SwapiFetchOperation(with: endpoint)
         let decode = SwapiDecodeOperation(for: endpoint.resource)
