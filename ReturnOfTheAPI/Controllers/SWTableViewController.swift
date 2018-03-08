aa359ceaa1dbd8975eaa0f53462507f213934e7f
Nathan Fulkerson <4764329+NFulkerson@users.noreply.github.com>
2018-03-08 22:29:56 +0900

    Moved TableViewDataSource logic for Starship, Vehicle, and Character into separate data source classes. 	- Realistically, starship and vehicle could belong to the same data source Added a quick and dirty currency converter accessory that appears under the relevant conditions (starships and vehicles) Generalized the segmented control that manages metric and imperial measures.

--- ReturnOfTheAPI/Controllers/SWTableViewController.swift
+++ ReturnOfTheAPI/Controllers/SWTableViewController.swift
@@ -12,6 +12,7 @@ import RealmSwift
 class SWTableViewController<Resource: ResourcePresentable>: UITableViewController {
 
     var resource: Resource
+    var dataSource: UITableViewDataSource
 
     fileprivate var quickFacts: QuickFactsView?
     let swDetailIdentifier = "swDetail"
@@ -19,6 +20,18 @@ class SWTableViewController<Resource: ResourcePresentable>: UITableViewControlle
 
     init(with resource: Resource) {
         self.resource = resource
+        if resource is Character {
+            dataSource = CharacterDetailDataSource(with: resource as! Character)
+        } else if resource is Vehicle {
+            dataSource = VehicleDetailDataSource(with: resource as! Vehicle)
+        } else if resource is Starship {
+            dataSource = StarshipDetailDataSource(with: resource as! Starship)
+        } else {
+            // ideally, we'd throw an error here and navigate back in our view hierarchy.
+            // need to research
+            fatalError("Resource is not a character, starship, or vehicle.")
+        }
+
         super.init(style: .grouped)
     }
 
@@ -52,175 +65,60 @@ class SWTableViewController<Resource: ResourcePresentable>: UITableViewControlle
         segmentControl.addTarget(self, action: #selector(unitMeasureChanged(segment:)), for: .valueChanged)
         let barButtonItem = UIBarButtonItem(customView: segmentControl)
         navigationItem.rightBarButtonItem = barButtonItem
+        if resource is Starship || resource is Vehicle {
+            addCurrencyButton()
+        }
         tableView.backgroundColor = .black
         tableView.delegate = self
-        tableView.dataSource = self
+        tableView.dataSource = dataSource
+        tableView.reloadData()
     }
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        if resource is Character {
-            return 4
-        }
-        return 3
+    private func addCurrencyButton() {
+        let currencyBarButtonItem = UIBarButtonItem(title: "$", style: .plain, target: self, action: #selector(currencyConversionSelected))
+        navigationItem.rightBarButtonItems?.append(currencyBarButtonItem)
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if resource is Character {
-            if let character = resource as? Character {
-                switch section {
-                case 0:
-                    return character.basicInfo.count
-                case 1:
-                    return character.films.count
-                case 2:
-                    return character.starships.count
-                case 3:
-                    return character.vehicles.count
-                default:
-                    return 0
-                }
-            }
-
-        } else if resource is Starship {
-            if let starship = resource as? Starship {
-                switch section {
-                case 0:
-                    return starship.basicInfo.count
-                case 1:
-                    return starship.films.count
-                case 2:
-                    return starship.pilots.count
-                default:
-                    return 0
-                }
-            }
-        } else if resource is Vehicle {
-            if let vehicle = resource as? Vehicle {
-                switch section {
-                case 0:
-                    return vehicle.basicInfo.count
-                case 1:
-                    return vehicle.films.count
-                case 2:
-                    return vehicle.pilots.count
-                default:
-                    return 0
-                }
-            }
+    @objc func currencyConversionSelected() {
+        let currencyView = UIAlertController(title: "Exchange Rate", message: "Enter exchange rate between USD and Galactic Credits.", preferredStyle: .alert)
+        currencyView.addTextField { (textField) in
+            textField.text = "1.0"
         }
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch section {
-        case 0:
-            return "Basic Info"
-        case 1:
-            return "Appears In"
-        case 2:
-            if resource is Character {
-                return "Starships"
+        currencyView.addAction(UIAlertAction(title: "Confirm", style: .default, handler: { [weak currencyView] (_) in
+            print("Hooray!: \(String(describing: currencyView?.textFields![0].text!))")
+            let input = currencyView?.textFields?[0].text
+            if self.resource is Valuable {
+                if let valu = self.resource as? Valuable, let inputRate = input, let rate = Double(inputRate) {
+                    if rate <= 0 {
+                        let error = SwapiError.invalidData(message: "Exchange rate cannot be negative or zero.")
+                        error.presentError()
+                        return
+                    }
+                    valu.convertedToDollars(valu.costInCredits, rate: rate)
+                    self.tableView.reloadData()
+                }
             }
-            return "Pilots"
-        case 3:
-            return "Vehicles"
-        default:
-            return nil
-        }
+        }))
+        present(currencyView, animated: true, completion: nil)
     }
 
     @objc private func unitMeasureChanged(segment: UISegmentedControl) {
         if segment.selectedSegmentIndex == 0 {
-            if let character = resource as? Character {
-                character.providesUnitsIn = .metric
-                self.tableView.reloadData()
+            if var measured = resource as? UnitProvider {
+                measured.providesUnitsIn = .metric
+                tableView.reloadData()
             }
         } else if segment.selectedSegmentIndex == 1 {
-            if let character = resource as? Character {
-                character.providesUnitsIn = .imperial
-                self.tableView.reloadData()
+            if var measured = resource as? UnitProvider {
+                measured.providesUnitsIn = .imperial
+                tableView.reloadData()
             }
         }
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell: UITableViewCell = {
-            if indexPath.section == 0 {
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: swDetailIdentifier) else {
-                    return UITableViewCell(style: .value1, reuseIdentifier: swDetailIdentifier)
-                }
-                return cell
-            } else {
-                guard let cell = tableView.dequeueReusableCell(withIdentifier: swSimpleIdentifier) else {
-                    return UITableViewCell(style: .default, reuseIdentifier: swSimpleIdentifier)
-                }
-                return cell
-            }
-        }()
-
-        cell.backgroundColor = .clear
-        cell.textLabel?.textColor = #colorLiteral(red: 1, green: 0.8431372549, blue: 0, alpha: 1)
-
-        let selectedCellView = UIView()
-        selectedCellView.backgroundColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
-        cell.selectedBackgroundView = selectedCellView
-        if let character = resource as? Character {
-            switch indexPath.section {
-            case 0:
-                cell.detailTextLabel?.text = character.basicInfo[indexPath.row].value as? String ?? ""
-                cell.detailTextLabel?.textColor = .white
-                cell.textLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
-                cell.textLabel?.text = character.basicInfo[indexPath.row].label
-                cell.textLabel?.textColor = UIColor(named: "BlueAccent")
-            case 1:
-                cell.textLabel?.text = character.films[indexPath.row].title
-            case 2:
-                cell.textLabel?.text = character.starships[indexPath.row].name
-            case 3:
-                cell.textLabel?.text = character.vehicles[indexPath.row].name
-            default:
-                cell.textLabel?.text = "Error?"
-            }
-        } else if let ship = resource as? Starship {
-            switch indexPath.section {
-            case 0:
-                cell.textLabel?.text = ship.basicInfo[indexPath.row].label
-                cell.textLabel?.textColor = UIColor(named: "BlueAccent")
-                cell.textLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
-
-                cell.detailTextLabel?.text = ship.basicInfo[indexPath.row].value as? String ?? ""
-                cell.detailTextLabel?.textColor = .white
-            case 1:
-                cell.textLabel?.text = ship.films[indexPath.row].title
-            case 2:
-                cell.textLabel?.text = ship.pilots[indexPath.row].name
-            default:
-                cell.textLabel?.text = "Error!"
-            }
-        } else if let vehicle = resource as? Vehicle {
-            switch indexPath.section {
-            case 0:
-                cell.textLabel?.text = vehicle.basicInfo[indexPath.row].label
-                cell.textLabel?.textColor = UIColor(named: "BlueAccent")
-                cell.textLabel?.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
-
-                cell.detailTextLabel?.text = vehicle.basicInfo[indexPath.row].value as? String ?? ""
-                cell.detailTextLabel?.textColor = .white
-            case 1:
-                cell.textLabel?.text = vehicle.films[indexPath.row].title
-            case 2:
-                cell.textLabel?.text = vehicle.pilots[indexPath.row].name
-            default:
-                cell.textLabel?.text = "Error!"
-            }
-        }
-
-        return cell
     }
 
     func setQuickFacts(resource: SwapiResource) {
         quickFacts = QuickFactsView(resource: resource)
     }
+
 
 }
